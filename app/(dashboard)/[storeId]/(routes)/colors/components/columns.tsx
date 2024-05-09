1bf3cb42627e83e2052bb80fe74834aef492b0bc
JackSlice2 <jackslice9@gmail.com>
2024-05-09 09:41:38 -0300

    Select component updated

--- app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
@@ -22,7 +22,7 @@ export const columns: ColumnDef<ColorColumn>[] = [
     cell: ({ row }) => (
       <div className="flex items-center gap-x-s">
         {row.original.value}
-        <div className="h-6 w-6 rounded-full border" 
+        <div className="h-6 w-6 rounded-full border"
         style={{ backgroundColor: row.original.value }} 
         />
       </div>
